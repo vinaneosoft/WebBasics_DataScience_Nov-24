ee7da7667202569e9beccb3a8bc6400b753c3e1e
vinaneosoft <vinaneosoft@gmail.com>
2024-12-03 13:40:09 +0530

    strings

--- JAVASCRIPT/strings.js
+++ JAVASCRIPT/strings.js
@@ -35,15 +35,26 @@ let myname="Veda";
 let concatination=myname.concat(" Sane");
 console.log(myname); // Veda or Veda Sane ? 
 console.log(concatination);
-
 let cname="Neosoft";
 cname=cname+", Rabale";
 console.log(cname); // Neosoft or Neosoft, Rabale??
-
 const empCode=45678;
 //empCode="neo-"+empCode; // Assignment to constant variable.
 //console.log(empCode);
-
 const eCode=45678;
 const newCode="neo-"+eCode; 
 console.log(newCode);
+
+// substring from current string
+
+message="India is my country. I love my India country"
+
+console.log( message.substring(0,4));// start index, end index
+console.log( message.slice(0,4)); // start index, end index
+console.log( message.substring(7));// start index
+console.log( message.slice(7)); // start index
+console.log( message.substring(-5));// -ve : 0 si
+console.log( message.slice(-5)); // -ve couting from end of the string
+console.log( message.substring(-5, 5));// -ve : 0 si, 5 ei
+console.log( message.slice(-5,5)); // blank string si must be smaller than ei
+console.log( message.slice(-25,30)); 
