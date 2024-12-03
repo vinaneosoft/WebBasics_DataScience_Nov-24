8fab242daf255db3c59ed24324f853a1fb6a1ce4
vinaneosoft <vinaneosoft@gmail.com>
2024-12-03 15:04:30 +0530

    arrays

---
+++ JAVASCRIPT/Arrays.js
@@ -0,0 +1,28 @@
+let salaries=[67900,5677889,787878,908990,576778];
+let names=["hari",'om',"pooja","kirti"];
+let array=[123,"hari",567899.4, true, 345, "pooja", 5678989, false];
+console.log(salaries.length);
+console.log(names.length);
+console.log(array.length);
+console.log(salaries);
+console.log(names);
+console.log(array);
+let empCodes=new Array(2); // 2 initial size
+console.log(empCodes.length);
+console.log(empCodes);
+empCodes.push(34566); // new element added at last, size grow
+console.log(empCodes.length);
+console.log(empCodes);
+empCodes[0]=4567;  // new element added to existing index
+empCodes[1]=23344;
+console.log(empCodes);
+empCodes[3]=4444;
+console.log(empCodes);
+empCodes[6]=4444;
+console.log(empCodes);
+empCodes.pop();
+console.log(empCodes);
+let codes=new Array(); // 0 initial size
+let ages=new Array(4,6);
+console.log(ages);
+
