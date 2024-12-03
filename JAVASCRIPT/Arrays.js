5b29fbf5d33f03be14a778ebbc6cc9baf2e38fd8
vinaneosoft <vinaneosoft@gmail.com>
2024-12-03 15:05:18 +0530

    arrays

--- JAVASCRIPT/Arrays.js
+++ JAVASCRIPT/Arrays.js
@@ -1,12 +1,15 @@
 let salaries=[67900,5677889,787878,908990,576778];
 let names=["hari",'om',"pooja","kirti"];
-let array=[123,"hari",567899.4, true, 345, "pooja", 5678989, false];
+let array=[123,"hari",567899.4, true, 345, "pooja", 5678989, false]
+
 console.log(salaries.length);
 console.log(names.length);
 console.log(array.length);
 console.log(salaries);
 console.log(names);
 console.log(array);
+
+
 let empCodes=new Array(2); // 2 initial size
 console.log(empCodes.length);
 console.log(empCodes);
@@ -22,7 +25,9 @@ empCodes[6]=4444;
 console.log(empCodes);
 empCodes.pop();
 console.log(empCodes);
+
 let codes=new Array(); // 0 initial size
+
 let ages=new Array(4,6);
 console.log(ages);
 
