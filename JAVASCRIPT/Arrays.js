9586c4d64056e97e9f21525255ab60bc04db5cb4
vinaneosoft <vinaneosoft@gmail.com>
2024-12-03 15:27:58 +0530

    arrays

--- JAVASCRIPT/Arrays.js
+++ JAVASCRIPT/Arrays.js
@@ -25,9 +25,32 @@ empCodes[6]=4444;
 console.log(empCodes);
 empCodes.pop();
 console.log(empCodes);
-
 let codes=new Array(); // 0 initial size
-
 let ages=new Array(4,6);
 console.log(ages);
 
+//stack, queue, deque
+names=["hari",'om',"pooja","kirti"];
+console.log(names[0][1]); //a
+console.log(names[1][0]);  //o
+
+names.push("krupa");
+names.unshift("Deepa");
+console.log(names);
+names.pop();
+names.shift();
+console.log(names);
+
+let joined=names.join("->")
+console.log(joined);
+
+// Arrays are mutable
+let ref=names.sort(); //Sorts an array in place. This method mutates the array and returns a reference to the same array.
+console.log(names);
+console.log(ref==names);
+
+ages=[28, 2, 34, 3, 56, 5, 47, 4];
+console.log(ages.sort());
+
+ages.sort((n1,n2)=>n1-n2)
+console.log(ages);
