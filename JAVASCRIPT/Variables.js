c74d50e5b3da8422d5ee839650da14116502baec
vinaneosoft <vinaneosoft@gmail.com>
2024-12-03 11:04:17 +0530

    variables

---
+++ JAVASCRIPT/Variables.js
@@ -0,0 +1,12 @@
+/*dynamically typed lang*/
+
+var salary
+salary=89000 // number
+document.write(typeof salary)
+salary="eight nine thousand"// string
+document.write(typeof salary)
+salary=true// boolean
+document.write(typeof salary)
+
+
+var companyName="Neosoft"; var companyCode=456777;
